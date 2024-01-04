3703e109b6dec8543f2be135705c3e7cbf7601a6
NC <nigelcheung97@hotmail.com>
2024-01-04 23:27:06 +0100

    added client authoritative movement with server authoritative movement snippet

--- Assets/NetworkPlayer.cs
+++ Assets/NetworkPlayer.cs
@@ -1,31 +1,38 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class NetworkPlayer : NetworkBehaviour
 {
-    [SerializeField] private Transform _playerCamera;
+    [SerializeField] private GameObject _playerCamera;
     [SerializeField] private float _movementSpeed;
+    [SerializeField, Range(0, 90)] private float _clampedYRotation;
 
     private float TargetYRotation { get; set; }
     
-    private Vector3 _movement;
-    private Vector2 _lookInput;
+    private MyPlayerInput _playerInput;
     private Rigidbody _rigidbody;
 
     public void Initialize()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _playerInput = new();
+        _playerInput.Enable();
+        if (IsLocalPlayer)
+        {
+            _playerCamera.GetComponent<Camera>().enabled = true;
+        }
+        
         if (IsOwner)
         {
             GetComponentInChildren<AudioListener>().enabled = true;
             if (Camera.main != null)
             {
-                Camera.main.gameObject.SetActive(false);
+                Camera.main.enabled = false;
             }
+
+            Cursor.visible = false;
         }
     }
 
@@ -34,52 +41,61 @@ public class NetworkPlayer : NetworkBehaviour
         base.OnNetworkSpawn();
         Initialize();
     }
-    
-    public void OnMove(InputValue value)
-    {
-        _movement = value.Get<Vector3>();
-    }
-    
-    public void OnLook(InputValue value)
-    {
-        _lookInput = value.Get<Vector2>();
-    }
 
     public void Update()
     {
-        if (!IsOwner)
-        {
-            return;
-        }
+        var moveInput = _playerInput.Player.Move.ReadValue<Vector3>();
+        var lookInput = _playerInput.Player.Look.ReadValue<Vector2>();
         
-        MovePlayerServerRpc();
-        RotatePlayerClient();
+        MovePlayer(moveInput);
+        RotatePlayer(lookInput);
+        
+        // todo: Server Authoritative Movement add with Client Side Prediction and Server Reconciliation (and Lag Compensation)
+        // if (IsServer && IsLocalPlayer)
+        // {
+        //     MovePlayer(moveInput);
+        //     RotatePlayer(lookInput);
+        // }
+        // else if (IsClient && IsLocalPlayer)
+        // {
+        //     MovePlayerServerRPC(moveInput);
+        //     RotatePlayerServerRPC(lookInput);
+        // }
     }
     
-    public void RotatePlayerClient()
+    private void MovePlayer(Vector3 movement)
+    {
+        var moveDirection = transform.right * movement.x + transform.forward * movement.z;
+        _rigidbody.MovePosition(transform.position + moveDirection * (_movementSpeed * Time.deltaTime));
+    }
+
+    [ServerRpc]
+    private void MovePlayerServerRPC(Vector3 movement)
+    {
+        MovePlayer(movement);
+    }
+    
+    private void RotatePlayer(Vector2 lookInput)
     {
         if (_playerCamera != null)
         {
-            TargetYRotation += _lookInput.y * 1;
-            var clampedYRotation = Mathf.Clamp(TargetYRotation, -50, 70);
-            _playerCamera.localRotation *= Quaternion.Euler(clampedYRotation, 0, 0);
+            TargetYRotation += lookInput.y * 1;
+    
+            if(TargetYRotation > _clampedYRotation)
+                TargetYRotation = _clampedYRotation;
+            else if(TargetYRotation < -_clampedYRotation)
+                TargetYRotation = -_clampedYRotation;
+    
+            var clampedYRotation = Mathf.Clamp(TargetYRotation, -50f, 70f);
+            _playerCamera.transform.localRotation = Quaternion.Euler(-clampedYRotation, 0, 0);
         }
         
-        transform.localRotation *= Quaternion.Euler(0, _lookInput.x, 0);
+        transform.localRotation *= Quaternion.Euler(0, lookInput.x, 0);
     }
-
-    // [ServerRpc]
-    // public void RotatePlayerServerRpc()
-    // {
-    //     transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.x + _lookInput.x, transform.localRotation.z);
-    // }
-
+    
     [ServerRpc]
-    public void MovePlayerServerRpc()
+    private void RotatePlayerServerRPC(Vector2 lookInput)
     {
-        // add direction into position
-        var moveDirection = transform.right * _movement.x + transform.forward * _movement.z;
-        Debug.Log(_movement);
-        _rigidbody.MovePosition(transform.position + moveDirection * (_movementSpeed * Time.deltaTime));
+        RotatePlayer(lookInput);
     }
 }
