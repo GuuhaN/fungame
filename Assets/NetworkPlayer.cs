20714d0d08573a719bc17bce48bdd39278f50e4d
NC <nigelcheung97@hotmail.com>
2024-01-04 10:07:14 +0100

    add rotation to the player

--- Assets/NetworkPlayer.cs
+++ Assets/NetworkPlayer.cs
@@ -7,9 +7,13 @@ using UnityEngine.InputSystem;
 
 public class NetworkPlayer : NetworkBehaviour
 {
+    [SerializeField] private Transform _playerCamera;
     [SerializeField] private float _movementSpeed;
 
+    private float TargetYRotation { get; set; }
+    
     private Vector3 _movement;
+    private Vector2 _lookInput;
     private Rigidbody _rigidbody;
 
     public void Initialize()
@@ -18,6 +22,10 @@ public class NetworkPlayer : NetworkBehaviour
         if (IsOwner)
         {
             GetComponentInChildren<AudioListener>().enabled = true;
+            if (Camera.main != null)
+            {
+                Camera.main.gameObject.SetActive(false);
+            }
         }
     }
 
@@ -26,16 +34,15 @@ public class NetworkPlayer : NetworkBehaviour
         base.OnNetworkSpawn();
         Initialize();
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     
     public void OnMove(InputValue value)
     {
         _movement = value.Get<Vector3>();
+    }
+    
+    public void OnLook(InputValue value)
+    {
+        _lookInput = value.Get<Vector2>();
     }
 
     public void Update()
@@ -46,11 +53,33 @@ public class NetworkPlayer : NetworkBehaviour
         }
         
         MovePlayerServerRpc();
+        RotatePlayerClient();
     }
+    
+    public void RotatePlayerClient()
+    {
+        if (_playerCamera != null)
+        {
+            TargetYRotation += _lookInput.y * 1;
+            var clampedYRotation = Mathf.Clamp(TargetYRotation, -50, 70);
+            _playerCamera.localRotation *= Quaternion.Euler(clampedYRotation, 0, 0);
+        }
+        
+        transform.localRotation *= Quaternion.Euler(0, _lookInput.x, 0);
+    }
+
+    // [ServerRpc]
+    // public void RotatePlayerServerRpc()
+    // {
+    //     transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.x + _lookInput.x, transform.localRotation.z);
+    // }
 
     [ServerRpc]
     public void MovePlayerServerRpc()
     {
-        _rigidbody.MovePosition(transform.position + _movement * (_movementSpeed * Time.deltaTime));
+        // add direction into position
+        var moveDirection = transform.right * _movement.x + transform.forward * _movement.z;
+        Debug.Log(_movement);
+        _rigidbody.MovePosition(transform.position + moveDirection * (_movementSpeed * Time.deltaTime));
     }
 }
